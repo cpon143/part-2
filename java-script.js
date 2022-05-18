4989fa9376292bbfd2735197aa41307cad7a1302
Md Mazharul Haque <cpon143@gmail.com>
2022-05-18 14:20:45 +0530

    function

--- java-script.js
+++ java-script.js
@@ -4,4 +4,27 @@ let hasDriversLicense = false;
 const passTest = true;
 
 if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log('sfdf');
+if (hasDriversLicense) console.log(`I can drive : D`);
+
+// const interface =audio;
+
+/********************Function************************************************ */
+//  calling   / running   /invoking
+
+function name(){
+    console.log('Md Mazharul Haque')
+}
+
+name();
+name();
+name();
+name();
+
+function fruitProcessor(apple,orange){
+    console.log(apple,orange)
+    console.log(`"Juice of ${apple} apple and ${orange} orange"`)
+}
+
+fruitProcessor(4,0)
+fruitProcessor(4,5)
+fruitProcessor(6,10)

e9bc5f667b1d0d11fd338212330a31f4720a4661
Md Mazharul Haque <cpon143@gmail.com>
2022-05-23 15:52:26 +0530

    new changes

--- java-script.js
+++ java-script.js
@@ -11,23 +11,51 @@ if (hasDriversLicense) console.log(`I can drive : D`);*/
 /********************Function************************************************ */
 //  calling   / running   /invoking
 
-function name() {
-    console.log('Md Mazharul Haque')
+// function name() {
+//     console.log('Md Mazharul Haque')
+// }
+
+// name();
+// name();
+// name();
+// name();
+
+// function fruitProcessor(apple, orange) {
+//     console.log(apple, orange)
+//     const juice = `Juice of ${apple} apple and ${orange} orange`;
+
+//     return  juice;
+// }
+
+// const appleJuice=fruitProcessor(5,0);
+// console.log(appleJuice);
+// const appleOrangeJuice=fruitProcessor(5,6);
+// console.log(appleOrangeJuice);
+
+// function decleration
+
+/*function calcAge(birthYear) {
+    return 2022 - birthYear;
 }
+const age1 = calcAge(2002);
+const age2 = calcAge(2020);
+console.log(age1, age2);*/
 
-name();
-name();
-name();
-name();
+// function expression
 
-function fruitProcessor(apple, orange) {
-    console.log(apple, orange)
-    const juice = `Juice of ${apple} apple and ${orange} orange`;
+/*const calcAge2 = function (birthYear) {
+    return 2022 - birthYear;
+}*/
 
-    return  juice;
+// Arrow function 
+
+const calcAge3 = birthYear => 2022 - birthYear;
+const age3 = calcAge3(2002);
+console.log(age3)
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2067 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement}`;
 }
-
-const appleJuice=fruitProcessor(5,0);
-console.log(appleJuice);
-const appleOrangeJuice=fruitProcessor(5,6);
-console.log(appleOrangeJuice);
+console.log(yearsUntilRetirement(2002,'Jonas'));

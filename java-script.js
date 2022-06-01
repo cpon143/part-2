964c24502d75a0fa881e8e2406b012695cfe2421
Md Mazharul Haque <cpon143@gmail.com>
2022-06-01 11:05:35 +0530

    new changes

--- java-script.js
+++ java-script.js
@@ -223,7 +223,7 @@ if (friends.includes('Vishal')) {
   actually be the returned value of a function! So you can just call a function as array
   values (so don't store the tip values in separate variables first, but right in the n
  */
-const calcTip = function (bill) {
+/*const calcTip = function (bill) {
     return bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
 }
 const bills = [125, 555, 44];
@@ -231,5 +231,11 @@ const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 // const bonus = [bills[0] + calcTip(bills[0]), bills[1] + calcTip(bills[1]), bills[2] + calcTip(bills[2])];
 const bonus = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 console.log(bills, tips, bonus);
-console.log('123' < 57);
+*/
 
+const jonas = {
+    firstName: 'Mazharul',
+    lastName:'Haque',
+    age:2022-2002,
+    friends:['Morning', 'Day', 'Evening'];
+}

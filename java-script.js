2b5873a7f02002548e3dab49e1acaac1cea882d9
Md Mazharul Haque <cpon143@gmail.com>
2022-06-01 10:37:30 +0530

    new changes

--- java-script.js
+++ java-script.js
@@ -228,7 +228,8 @@ const calcTip = function (bill) {
 }
 const bills = [125, 555, 44];
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-const bonus = [bills[0] + calcTip(bills[0]), bills[1] + calcTip(bills[1]), bills[2] + calcTip(bills[2])];
-console.log(bills, tips,);
-// const bonus = [bills(0) + tips(0),bills(0) + tips(0),bills(0) + tips(0)]; 
-console.log(bonus);
+// const bonus = [bills[0] + calcTip(bills[0]), bills[1] + calcTip(bills[1]), bills[2] + calcTip(bills[2])];
+const bonus = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(bills, tips, bonus);
+console.log('123' < 57);
+

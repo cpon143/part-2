289374ec4a5460db08b2fba0c3164f1315d65df4
Md Mazharul Haque <cpon143@gmail.com>
2022-06-15 12:58:18 +0530

    new changes

--- java-script.js
+++ java-script.js
@@ -437,7 +437,7 @@ let dice = Math.trunc(Math.random() * 6) + 1;
 while (dice !== 6) {
     console.log(`You can rolling dice ${dice}`);
     dice = Math.trunc(Math.random() * 6) + 1;
-    if(dice===6){
+    if (dice === 6) {
         console.log('End of the loop');
     }
 }
